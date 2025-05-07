2c95e16b5f839dd605bb890761a4d60ad76c0231
TM-Tareq <tmtareq20@gmail.com>
2025-05-07 15:06:58 +0600

    todays starting code

--- app.js
+++ app.js
@@ -24,10 +24,52 @@ for(const btn of allBtn) {
         selectedContainer.appendChild(li);
         li.style.display = 'flex';
 
+        // const totalCost = document.getElementById('total-cost').innerText;
+
+        // const convertedTotalCost = parseInt(totalCost);
+
+        // const sum = convertedTotalCost + parseInt(price);
+
+        // setInnerText('total-cost', sum);
+
+        totalCost('total-cost', parseInt(price))
+
+
+        // here the grand total section
+        // const grandTotal = document.getElementById('grand-total').innerText;
+        // const convertedGrandTotal = parseInt(grandTotal);
+        // const sum2 = convertedGrandTotal + parseInt(price);
+        // console.log(sum2)
+    
+        // setInnerText('grand-total', sum2);
+
+        // grandTotalCost("grand-total", parseInt(price));
+
+        grandTotalCost('grand-total', parseInt(price));
+
         setInnerText('cart-count', count);
     });
 }
 
+
+function totalCost(id, value) {
+    const totalCost = document.getElementById(id).innerText;
+
+    const convertedTotalCost = parseInt(totalCost);
+
+    const sum = convertedTotalCost + parseInt(value);
+
+    setInnerText(id, sum);
+}
+
+function grandTotalCost() {
+    const grandTotal = document.getElementById('grand-total').innerText;
+    const convertedTotalCost = parseInt(grandTotal);
+
+    const sum2 = convertedTotalCost + parseInt(totalCost);
+
+    setInnerText('grand-total', sum2);
+}
 
 
 function setInnerText(id, value) {
